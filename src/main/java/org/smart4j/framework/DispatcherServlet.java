743f0179bbbe691b74e669918a247e2d9685b0a0
zhangxinzheng0812 <programer.email@gmail.com>
2016-02-25 18:06:56 +0800

    update

--- src/main/java/org/smart4j/framework/DispatcherServlet.java
+++ src/main/java/org/smart4j/framework/DispatcherServlet.java
@@ -1,13 +1,9 @@
 package org.smart4j.framework;
 
-import org.apache.commons.dbutils.handlers.BeanHandler;
-import org.smart4j.framework.bean.Data;
-import org.smart4j.framework.bean.Handler;
-import org.smart4j.framework.bean.Param;
-import org.smart4j.framework.bean.View;
-import org.smart4j.framework.helper.*;
-import org.smart4j.framework.util.*;
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.util.Map;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -16,77 +12,73 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.reflect.Method;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
+import org.smart4j.framework.bean.Data;
+import org.smart4j.framework.bean.Handler;
+import org.smart4j.framework.bean.Param;
+import org.smart4j.framework.bean.View;
+import org.smart4j.framework.helper.BeanHelper;
+import org.smart4j.framework.helper.ConfigHelper;
+import org.smart4j.framework.helper.ControllerHelper;
+import org.smart4j.framework.helper.RequestHelper;
+import org.smart4j.framework.helper.ServletHelper;
+import org.smart4j.framework.helper.UploadHelper;
+import org.smart4j.framework.util.JsonUtil;
+import org.smart4j.framework.util.ReflectionUtil;
+import org.smart4j.framework.util.StringUtil;
 
 /**
- * Created by yzy on 2016/2/23.
+ * 请求转发器
  */
 @WebServlet(urlPatterns = "/*", loadOnStartup = 0)
 public class DispatcherServlet extends HttpServlet {
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        HelperLoader.init();
-        ServletContext servletContext = config.getServletContext();
-        ServletRegistration jspServlet = servletContext.getServletRegistration("jsp");
-        jspServlet.addMapping(ConfigHelper.getAppJspPath() + "*");
 
-        ServletRegistration defaultServlet = servletContext.getServletRegistration("default");
-        defaultServlet.addMapping(ConfigHelper.getAppAssetPath() + "*");
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        HelperLoader.init();
+
+        ServletContext servletContext = servletConfig.getServletContext();
+
+        registerServlet(servletContext);
 
         UploadHelper.init(servletContext);
     }
 
+    private void registerServlet(ServletContext servletContext) {
+        ServletRegistration jspServlet = servletContext.getServletRegistration("jsp");
+        jspServlet.addMapping("/index.jsp");
+        jspServlet.addMapping(ConfigHelper.getAppJspPath() + "*");
+
+        ServletRegistration defaultServlet = servletContext.getServletRegistration("default");
+        defaultServlet.addMapping("/favicon.ico");
+        defaultServlet.addMapping(ConfigHelper.getAppAssetPath() + "*");
+    }
+
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ServletHelper.init(req,resp);
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletHelper.init(request, response);
         try {
-            String requestMethod = req.getMethod().toLowerCase();
-            String requestPath = req.getPathInfo();
+            String requestMethod = request.getMethod().toLowerCase();
+            String requestPath = request.getPathInfo();
             Handler handler = ControllerHelper.getHandler(requestMethod, requestPath);
-            if (null != handler) {
+            if (handler != null) {
                 Class<?> controllerClass = handler.getControllerClass();
                 Object controllerBean = BeanHelper.getBean(controllerClass);
 
-                Map<String, Object> paramsMap = new HashMap<String, Object>();
-                Enumeration<String> paramNames = req.getParameterNames();
-                while (paramNames.hasMoreElements()) {
-                    String paramName = paramNames.nextElement();
-                    String paramValue = req.getParameter(paramName);
-                    paramsMap.put(paramName, paramValue);
-                }
-                String body = CodecUtil.decodeURL(StreamUtil.getString(req.getInputStream()));
-                if (StringUtil.isNotEmpty(body)) {
-                    String[] params = StringUtil.spiltString(body, "&");
-                    if (ArrayUtil.isNotEmpty(params)) {
-                        for (String param : params) {
-                            String[] array = StringUtil.spiltString(param, "=");
-                            if (ArrayUtil.isNotEmpty(array) && array.length == 2) {
-                                String paramName = array[0];
-                                String paramValue = array[1];
-                                paramsMap.put(paramName, paramValue);
-                            }
-                        }
-                    }
-                }
-                Param param=null;
-                if (UploadHelper.isMultipart(req)) {
+                Param param;
+                if (UploadHelper.isMultipart(request)) {
                     try {
-                        param = UploadHelper.createParam(req);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                        param = UploadHelper.createParam(request);
+                    }catch (Exception e){
+                        throw new RuntimeException(e);
                     }
                 } else {
                     try {
-                        param = RequestHelper.createParam(req);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                        param = RequestHelper.createParam(request);
+                    }catch (Exception e){
+                        throw new RuntimeException(e);
                     }
                 }
+
                 Object result;
                 Method actionMethod = handler.getActionMethod();
                 if (param.isEmpty()) {
@@ -94,36 +86,43 @@ public class DispatcherServlet extends HttpServlet {
                 } else {
                     result = ReflectionUtil.invokeMethod(controllerBean, actionMethod, param);
                 }
+
                 if (result instanceof View) {
-                    View view = (View) result;
-                    String path = view.getPath();
-                    if (StringUtil.isNotEmpty(path)) {
-                        if (path.startsWith("/")) {
-                            resp.sendRedirect(req.getContextPath() + path);
-                        } else {
-                            Map<String, Object> model = view.getModel();
-                            for (Map.Entry<String, Object> entry : model.entrySet()) {
-                                req.setAttribute(entry.getKey(), entry.getValue());
-                            }
-                            req.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(req, resp);
-                        }
-                    } else if (result instanceof Data) {
-                        Data data = (Data) result;
-                        Object model = data.getModel();
-                        if (null != model) {
-                            resp.setContentType("application/json");
-                            resp.setCharacterEncoding("UTF-8");
-                            PrintWriter writer = resp.getWriter();
-                            String json = JsonUtil.toJson(model);
-                            writer.write(json);
-                            writer.flush();
-                            writer.close();
-                        }
-                    }
+                    handleViewResult((View) result, request, response);
+                } else if (result instanceof Data) {
+                    handleDataResult((Data) result, response);
                 }
             }
         } finally {
             ServletHelper.destory();
+        }
+    }
+
+    private void handleViewResult(View view, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String path = view.getPath();
+        if (StringUtil.isNotEmpty(path)) {
+            if (path.startsWith("/")) {
+                response.sendRedirect(request.getContextPath() + path);
+            } else {
+                Map<String, Object> model = view.getModel();
+                for (Map.Entry<String, Object> entry : model.entrySet()) {
+                    request.setAttribute(entry.getKey(), entry.getValue());
+                }
+                request.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(request, response);
+            }
+        }
+    }
+
+    private void handleDataResult(Data data, HttpServletResponse response) throws IOException {
+        Object model = data.getModel();
+        if (model != null) {
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter writer = response.getWriter();
+            String json = JsonUtil.toJson(model);
+            writer.write(json);
+            writer.flush();
+            writer.close();
         }
     }
 }
