ba912d442f005a09962ef78c590430d0d4db6820
zhangxinzheng0812 <programer.email@gmail.com>
2016-02-25 17:05:21 +0800

    servle解耦

--- src/main/java/org/smart4j/framework/DispatcherServlet.java
+++ src/main/java/org/smart4j/framework/DispatcherServlet.java
@@ -43,82 +43,87 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String requestMethod = req.getMethod().toLowerCase();
-        String requestPath = req.getPathInfo();
-        Handler handler = ControllerHelper.getHandler(requestMethod, requestPath);
-        if (null != handler) {
-            Class<?> controllerClass = handler.getControllerClass();
-            Object controllerBean = BeanHelper.getBean(controllerClass);
+        ServletHelper.init(req,resp);
+        try {
+            String requestMethod = req.getMethod().toLowerCase();
+            String requestPath = req.getPathInfo();
+            Handler handler = ControllerHelper.getHandler(requestMethod, requestPath);
+            if (null != handler) {
+                Class<?> controllerClass = handler.getControllerClass();
+                Object controllerBean = BeanHelper.getBean(controllerClass);
 
-            Map<String, Object> paramsMap = new HashMap<String, Object>();
-            Enumeration<String> paramNames = req.getParameterNames();
-            while (paramNames.hasMoreElements()) {
-                String paramName = paramNames.nextElement();
-                String paramValue = req.getParameter(paramName);
-                paramsMap.put(paramName, paramValue);
-            }
-            String body = CodecUtil.decodeURL(StreamUtil.getString(req.getInputStream()));
-            if (StringUtil.isNotEmpty(body)) {
-                String[] params = StringUtil.spiltString(body, "&");
-                if (ArrayUtil.isNotEmpty(params)) {
-                    for (String param : params) {
-                        String[] array = StringUtil.spiltString(param, "=");
-                        if (ArrayUtil.isNotEmpty(array) && array.length == 2) {
-                            String paramName = array[0];
-                            String paramValue = array[1];
-                            paramsMap.put(paramName, paramValue);
+                Map<String, Object> paramsMap = new HashMap<String, Object>();
+                Enumeration<String> paramNames = req.getParameterNames();
+                while (paramNames.hasMoreElements()) {
+                    String paramName = paramNames.nextElement();
+                    String paramValue = req.getParameter(paramName);
+                    paramsMap.put(paramName, paramValue);
+                }
+                String body = CodecUtil.decodeURL(StreamUtil.getString(req.getInputStream()));
+                if (StringUtil.isNotEmpty(body)) {
+                    String[] params = StringUtil.spiltString(body, "&");
+                    if (ArrayUtil.isNotEmpty(params)) {
+                        for (String param : params) {
+                            String[] array = StringUtil.spiltString(param, "=");
+                            if (ArrayUtil.isNotEmpty(array) && array.length == 2) {
+                                String paramName = array[0];
+                                String paramValue = array[1];
+                                paramsMap.put(paramName, paramValue);
+                            }
+                        }
+                    }
+                }
+                Param param=null;
+                if (UploadHelper.isMultipart(req)) {
+                    try {
+                        param = UploadHelper.createParam(req);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        param = RequestHelper.createParam(req);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                Object result;
+                Method actionMethod = handler.getActionMethod();
+                if (param.isEmpty()) {
+                    result = ReflectionUtil.invokeMethod(controllerBean, actionMethod);
+                } else {
+                    result = ReflectionUtil.invokeMethod(controllerBean, actionMethod, param);
+                }
+                if (result instanceof View) {
+                    View view = (View) result;
+                    String path = view.getPath();
+                    if (StringUtil.isNotEmpty(path)) {
+                        if (path.startsWith("/")) {
+                            resp.sendRedirect(req.getContextPath() + path);
+                        } else {
+                            Map<String, Object> model = view.getModel();
+                            for (Map.Entry<String, Object> entry : model.entrySet()) {
+                                req.setAttribute(entry.getKey(), entry.getValue());
+                            }
+                            req.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(req, resp);
+                        }
+                    } else if (result instanceof Data) {
+                        Data data = (Data) result;
+                        Object model = data.getModel();
+                        if (null != model) {
+                            resp.setContentType("application/json");
+                            resp.setCharacterEncoding("UTF-8");
+                            PrintWriter writer = resp.getWriter();
+                            String json = JsonUtil.toJson(model);
+                            writer.write(json);
+                            writer.flush();
+                            writer.close();
                         }
                     }
                 }
             }
-            Param param=null;
-            if (UploadHelper.isMultipart(req)) {
-                try {
-                    param = UploadHelper.createParam(req);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else {
-                try {
-                    param = RequestHelper.createParam(req);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            Object result;
-            Method actionMethod = handler.getActionMethod();
-            if (param.isEmpty()) {
-                result = ReflectionUtil.invokeMethod(controllerBean, actionMethod);
-            } else {
-                result = ReflectionUtil.invokeMethod(controllerBean, actionMethod, param);
-            }
-            if (result instanceof View) {
-                View view = (View) result;
-                String path = view.getPath();
-                if (StringUtil.isNotEmpty(path)) {
-                    if (path.startsWith("/")) {
-                        resp.sendRedirect(req.getContextPath() + path);
-                    } else {
-                        Map<String, Object> model = view.getModel();
-                        for (Map.Entry<String, Object> entry : model.entrySet()) {
-                            req.setAttribute(entry.getKey(), entry.getValue());
-                        }
-                        req.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(req, resp);
-                    }
-                } else if (result instanceof Data) {
-                    Data data = (Data) result;
-                    Object model = data.getModel();
-                    if (null != model) {
-                        resp.setContentType("application/json");
-                        resp.setCharacterEncoding("UTF-8");
-                        PrintWriter writer = resp.getWriter();
-                        String json = JsonUtil.toJson(model);
-                        writer.write(json);
-                        writer.flush();
-                        writer.close();
-                    }
-                }
-            }
+        } finally {
+            ServletHelper.destory();
         }
     }
 }
---
+++ src/main/java/org/smart4j/framework/helper/ServletHelper.java
@@ -0,0 +1,88 @@
+package org.smart4j.framework.helper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by yzy on 2016/2/25.
+ */
+public final class ServletHelper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServletHelper.class);
+
+    private static final ThreadLocal<ServletHelper> SERVLET_HELPER_HOLDER = new ThreadLocal<ServletHelper>();
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+
+    public ServletHelper(HttpServletRequest request, HttpServletResponse response) {
+        this.request = request;
+        this.response = response;
+    }
+
+    public static void init(HttpServletRequest request, HttpServletResponse response){
+        SERVLET_HELPER_HOLDER.set(new ServletHelper(request,response));
+    }
+
+    public static void destory(){
+        SERVLET_HELPER_HOLDER.remove();
+    }
+
+    private static HttpServletRequest getRequest(){
+        return SERVLET_HELPER_HOLDER.get().request;
+    }
+
+    private static HttpServletResponse getResponse(){
+        return SERVLET_HELPER_HOLDER.get().response;
+    }
+
+    private static HttpSession getSession(){
+        return getRequest().getSession();
+    }
+
+    private static ServletContext getServletContext(){
+        return getRequest().getServletContext();
+    }
+
+    public static void setRequestAttribute(String key, Object value){
+        getRequest().setAttribute(key, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getRequestAttribute(String key){
+        return (T)getRequest().getAttribute(key);
+    }
+
+    public static void remoteRequestAttribute(String key){
+        getRequest().removeAttribute(key);
+    }
+
+    public static void sendRedirect(String location){
+        try {
+            getResponse().sendRedirect(getRequest().getContextPath() + location);
+        } catch (IOException e){
+            LOGGER.error("redirect failure", e);
+        }
+    }
+
+    public static void setSessionAttribute(String key, Object value){
+        getSession().setAttribute(key,value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getSessionAttribute(String key){
+       return (T)getRequest().getSession().getAttribute(key);
+    }
+
+    public static void removeSessionAttriubte(String key){
+        getRequest().getSession().removeAttribute(key);
+    }
+
+    public static void invalidateSession(){
+        getRequest().getSession().invalidate();
+    }
+}
